6cfde80d52dde8b6ab92c5ca3cec39f680deead9
Rojehag <jenz.rojehag@icloud.com>
2025-01-09 14:19:38 +0100

    Skriptet för melee funka inte

--- ForrestMaze/Assets/Scripts/Player/PlayerAttack.cs
+++ ForrestMaze/Assets/Scripts/Player/PlayerAttack.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private float timeBetweenAttack;
-    private float startTimeBetweenAttack;
-
-    public Transform attackPosition;
-    public LayerMask whatIsEnemys;
-    public float attackRange;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +16,6 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timeBetweenAttack <= 0)
-        {
-            // Then you can attack
-            if (Input.GetKey(KeyCode.Mouse1))
-            {
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemys); 
-            }
-            timeBetweenAttack = startTimeBetweenAttack;
-        }
-        else
-        {
-            timeBetweenAttack -= Time.deltaTime;
-        }
+       
     }
 }
